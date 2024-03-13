f2aac8e37f1f6a2ea7b33c6ad794cd3d5f739333
KateDV8 <pavlovae8@gmail.com>
2024-03-13 11:29:07 +0500

    creat: class AnalysisArray

---
+++ src/main/java/ru/ekaterinadvoretskaia/arraytaskn5/AnalysisArray.java
@@ -0,0 +1,16 @@
+package ru.ekaterinadvoretskaia.arraytaskn5;
+
+public class AnalysisArray {
+
+
+    // 1.number of even numbers
+    // 2.number of odd numbers
+    // 3.number of prime numbers
+    // 4.sum of all numbers in array
+    // 5.difference between sum of even index  and odd index numbers
+    // 6.quantity of values (0)
+
+
+
+
+}
---
+++ src/main/java/ru/ekaterinadvoretskaia/arraytaskn5/_Main.java
@@ -0,0 +1,15 @@
+package ru.ekaterinadvoretskaia.arraytaskn5;
+
+import java.util.Scanner;
+
+public class _Main {
+    public static void main(String[] args) {
+        // Scanner sc;
+        //   sc = new Scanner(System.in);
+
+        int[] arr = new int[]{5, -3, 2, 1, 7, 11, 12, -4, 1, 10};
+        for (int i = 0; i < 10; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
